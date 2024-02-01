3b573e1b3f59f9cfd43ed839eb5ea57eeba03c27
professeur01 <awaloumassaoudou@gmail.com>
2024-02-01 15:37:31 -0800

    3 exo

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,4 @@
-#include  <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
@@ -30,7 +30,6 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 		return (new);
 	}
-	
 	while (temp->next)
 		temp = temp->next;
 

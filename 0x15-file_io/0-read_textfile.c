c434dc045102db38e93366d14f5847d209bbd832
professeur01 <awaloumassaoudou@gmail.com>
2024-02-16 18:14:31 -0800

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,10 +6,10 @@
  * @filename: text file being read
  * @letters: number of letters to be read
  * Return: w- actual number of bytes read abd printed
- * 	0 when function fails or filename is NULL.
+ *	0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
-{	
+{
 	char *buf;
 	ssize_t fd;
 	ssize_t w;
